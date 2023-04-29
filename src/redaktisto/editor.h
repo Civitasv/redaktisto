27cf491bb9b9fccdead2a3e0c37f8e77dd2eb0a6
Civitasv <hscivitasv@gmail.com>
2023-04-29 16:18:35 +0800

    makefile -> CMake

---
+++ src/redaktisto/editor.h
@@ -0,0 +1,6 @@
+#ifndef REDAKTISTO_EDITOR_H_
+#define REDAKTISTO_EDITOR_H_
+
+void redaktisto_editor();
+
+#endif  // REDAKTISTO_EDITOR_H_
---
+++ src/run.c
@@ -0,0 +1,7 @@
+#include "redaktisto/editor.h"
+#include <stdio.h>
+
+int main() {
+	printf("%s", "this is redaktisto");
+	return 0;
+}
